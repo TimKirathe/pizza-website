03ce78f5077ec1340a17f6435f5902f99204906d
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-31 09:47:29 +0300

    Addition of for and if loops in the js code in order to see which checkboxes are ticked when choosing toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-
-
-
-
-
-
-
-
-
+// Business Logic
+function NewPizza(size, crust) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = [];
+}
 
 
 
@@ -15,7 +12,7 @@
 $(document).ready(function() {
   $("#newPizza").click(function() {
     $("#pizzas").append('<div class="form-group">' +
-      '<label for="pizzaSize" class="display-4">Please select your pizza size:</label>' +
+      '<label for="pizzaSize" class="display-6">Please select your pizza size:</label>' +
       '<select class="form-control" id="pizzaSize">' +
         '<option id="small" value="1">Small</option>' +
         '<option id="medium" value="2">Medium</option>' +
@@ -23,7 +20,7 @@ $(document).ready(function() {
       '</select>' +
     '</div>' +
     '<div class="form-group">' +
-      '<label for="crustType" class="display-4">Please select the type of crust you would like:</label>' +
+      '<label for="crustType" class="display-6">Please select the type of crust you would like:</label>' +
       '<select class="form-control" id="crustType">' +
         '<option id="stuffed" value="1">Stuffed Crust</option>' +
         '<option id="flatBread" value="2">Flat Bread Crust</option>' +
@@ -32,7 +29,7 @@ $(document).ready(function() {
       '</select>' +
     '</div>' +
     '<div class="form-group">' +
-      '<label for="toppings" class="display-4">Please select the toppings you would like on your pizza:</label>' +
+      '<label for="toppings" class="display-6">Please select the toppings you would like on your pizza:</label>' +
       '<div id="toppings">' +
         '<input class="" type="checkbox" id="pepperoni" /> Pepperoni' +
         '<br>' +
@@ -51,5 +48,24 @@ $(document).ready(function() {
         '<input class="" type="checkbox" id="beef" /> Beef' +
       '</div>' +
     '</div>');
+  });
+  $("#pizzaInput").submit(function(event) {
+    event.preventDefault();
+
+    var pizzaSize = $("#pizzaSize").val();
+    /*alert(pizzaSize);*/
+    var pizzaCrust = $("#crustType").val();
+    /*alert(pizzaCrust);*/
+    var pizzaOrdered = new NewPizza(pizzaSize, pizzaCrust);
+    var checkboxField = document.forms[0].topping;
+    console.log(checkboxField, typeof(checkboxField));
+    for (var i=0; i<checkboxField.length; i++) {
+      if (checkboxField[i].checked) {
+        pizzaOrdered.toppings.push(checkboxField[i].value);
+        console.log(pizzaOrdered.toppings);
+        /*alert("The selected option was " + checkboxField[i].value);*/
+      }
+    }
+    $(".checkoutPage").show(600, "linear");
   });
 });
