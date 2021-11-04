300212e26ec81fb2a2a7790b3e94e97fad996f6e
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-04 23:08:46 +0300

    Added an if loop used to check the values of the toppings chosen and link them to their certain names

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,9 @@ function Pizza(size, crust, toppings, orderNo, total) {
 }
 
 var pizzaToppings = [];
+var pizzaToppingsNames = [];
 var toppingsCost = 0;
+var grandTotal = 0;
 
 var deliveryCost = 200;
 
@@ -21,23 +23,54 @@ $(document).ready(function() {
     event.preventDefault();
 
       let pizzaSize = $("#pizzaSize").val();
-      alert(pizzaSize);
       let pizzaCrust = $("#pizzaCrust").val();
-      alert(pizzaCrust);
 
       $.each($('input[name="toppings"]:checked'), function() {
         pizzaToppings.push($(this).val());
       });
-      alert(pizzaToppings);
       for (var i=0; i<pizzaToppings.length; i++) {
+        if (pizzaToppings[i] === "100") {
+          pizzaToppingsNames.push("Pepperoni")
+        }
+        else if (pizzaToppings[i] === "80") {
+          pizzaToppingsNames.push("Mushrooms")
+        }
+        else if (pizzaToppings[i] === "150") {
+          pizzaToppingsNames.push("Bacon")
+        }
+        else if (pizzaToppings[i] === "100") {
+          pizzaToppingsNames.push("Green Pepper")
+        }
+        else if (pizzaToppings[i] === "70") {
+          pizzaToppingsNames.push("Extra Chees")
+        }
+        else if (pizzaToppings[i] === "90") {
+          pizzaToppingsNames.push("Pineapple")
+        }
+        else if (pizzaToppings[i] === "200") {
+          pizzaToppingsNames.push("Beef")
+        }
+        else if (pizzaToppings[i] === "210") {
+          pizzaToppingsNames.push("Ham")
+        }
+        else if (pizzaToppings[i] === "110") {
+          pizzaToppingsNames.push("Chicken")
+        }
         toppingsCost += parseInt(pizzaToppings[i]);
       }
-      alert(toppingsCost);
+
       var total = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(toppingsCost);
       var orderNo = 1;
 
+      $("#orderButton").hide();
       $("#anotherOrderButton").show();
       $(".priceDisplay").show();
+      $("#orderNo").html('OrderNo: ' + orderNo + ' | ');
+      $("#sizeOfPizza").html('Pizza size: ' + $("#pizzaSize option:selected").text() + ' | ');
+      $("#crust").html('Crust: ' + $("#pizzaCrust option:selected").text() + ' | ');
+      $("#toppingsChosen").html('Toppings Chosen: ' + $.each($('input[name="toppings"]:checked').siblings().text()) + ' | ');
+      $("#totalAmount").html('Total: ' + total + ' | ');
 
+      $("#orderSubmit")[0].reset();
   });
 });
