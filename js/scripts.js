09d0e921fdd23c60198d296ce5779b7b849123db
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-04 19:37:42 +0300

    Created the various varibles needed to calculate the cost of the first pizza

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,37 @@ function Pizza(size, crust, toppings, orderNo, total) {
   this.size = total;
 }
 
+var pizzaToppings = [];
+var toppingsCost = 0;
+
 var deliveryCost = 200;
 
 
 // User Interface
 $(document).ready(function() {
-  $("#anotherOrderButton").hide();
+  $('#anotherOrderButton, .priceDisplay').hide();
+
   $("#orderSubmit").submit(function(event) {
     event.preventDefault();
-    $("#orderButton").click(function() {
-      let pizzaCost = $("#pizzaCrust")
-    });
+
+      let pizzaSize = $("#pizzaSize").val();
+      alert(pizzaSize);
+      let pizzaCrust = $("#pizzaCrust").val();
+      alert(pizzaCrust);
+
+      $.each($('input[name="toppings"]:checked'), function() {
+        pizzaToppings.push($(this).val());
+      });
+      alert(pizzaToppings);
+      for (var i=0; i<pizzaToppings.length; i++) {
+        toppingsCost += parseInt(pizzaToppings[i]);
+      }
+      alert(toppingsCost);
+      var total = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(toppingsCost);
+      var orderNo = 1;
+
+      $("#anotherOrderButton").show();
+      $(".priceDisplay").show();
+
   });
 });
