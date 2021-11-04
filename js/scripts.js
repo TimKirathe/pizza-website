2671ddf4bf5e8479bc8b166b5313b15a4f5e39c4
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-04 18:26:27 +0300

    Started working on the scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic
+function Pizza(size, crust, toppings, orderNo, total) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = [];
+  this.orderNo = orderNo;
+  this.size = total;
+}
+
+var deliveryCost = 200;
