d32b588f43fcbb37d99be0dcaf83f9af4f1f9406
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-05 18:51:32 +0300

    Finalized the styling and javascript of the webpage. About to deploy it to gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
 
   $("#startOrderButton").click(function() {
     $(".orderingSection").show();
+    $(".gallery").hide();
 
     $("#orderButton").click(function() {
 
@@ -77,6 +78,63 @@ $(document).ready(function() {
       $("#totalAmount").html('Total: ' + total);
       $("#grandTotal").html('Grand total: ' + grandTotal);
 
+      $("#anotherOrderButton").click(function() {
+        orderNo = orderNo + 1;
+
+        pizzaToppingsNames.splice(0, 9);
+        pizzaToppings.splice(0, 9);
+
+        let pizzaSize = $("#pizzaSize").val();
+        let pizzaCrust = $("#pizzaCrust").val();
+
+
+        $.each($('input[name="toppings"]:checked'), function() {
+          pizzaToppings.push($(this).val());
+        });
+
+        for (var i=0; i<pizzaToppings.length; i++) {
+          if (pizzaToppings[i] === "100") {
+            pizzaToppingsNames.push("Pepperoni")
+          }
+          else if (pizzaToppings[i] === "80") {
+            pizzaToppingsNames.push("Mushrooms")
+          }
+          else if (pizzaToppings[i] === "150") {
+            pizzaToppingsNames.push("Bacon")
+          }
+          else if (pizzaToppings[i] === "100") {
+            pizzaToppingsNames.push("Green Pepper")
+          }
+          else if (pizzaToppings[i] === "70") {
+            pizzaToppingsNames.push("Extra Cheese")
+          }
+          else if (pizzaToppings[i] === "90") {
+            pizzaToppingsNames.push("Pineapple")
+          }
+          else if (pizzaToppings[i] === "200") {
+            pizzaToppingsNames.push("Beef")
+          }
+          else if (pizzaToppings[i] === "210") {
+            pizzaToppingsNames.push("Ham")
+          }
+          else if (pizzaToppings[i] === "110") {
+            pizzaToppingsNames.push("Chicken")
+          }
+          toppingsCost += parseInt(pizzaToppings[i]);
+        }
+
+        var total = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(toppingsCost);
+        grandTotal += total;
+
+        let anotherPizza = new Pizza(pizzaSize, pizzaCrust, orderNo, total);
+
+        let anotherPizzaOrder = '<p>' + '<span id="orderNo">' + 'Order No: ' + anotherPizza.orderNo + ' | ' + '</span>' + '<span id="sizeOfPizza">' + 'Pizza Size: ' + $("#pizzaSize option:selected").text() + ' | ' + '</span>' + '<span id="crust">' + 'Pizza Crust: ' + $("#pizzaCrust option:selected").text() + ' | ' + '</span>' + '<span id="toppingsChosen">' + 'Toppings Chosen: ' + pizzaToppingsNames.join(", ") + ' | ' + '</span>' + '<span id="totalAmount">' + 'Total: ' + anotherPizza.total + '</span>' + '</p>'
+
+        $(".priceDisplay").append(anotherPizzaOrder);
+        $("#grandTotal").html('Grand total: ' + grandTotal);
+
+      });
+
       $("#checkoutButton").click(function() {
         var delivery = prompt("Would you like your pizza(s) delivered? There will be an additional cost of ksh. 200 upon delivery - Y / n");
 
@@ -88,63 +146,6 @@ $(document).ready(function() {
         else if (delivery === "n") {
           alert("Dear Esteemed Customer, thank you for your order. We look forward to serving you again soon. Your total amount to be paid is " + grandTotal);
         }
-
-        $("#anotherOrderButton").click(function() {
-          orderNo = orderNo + 1;
-
-          pizzaToppingsNames.splice(0, 9);
-          pizzaToppings.splice(0, 9);
-
-          let pizzaSize = $("#pizzaSize").val();
-          let pizzaCrust = $("#pizzaCrust").val();
-
-
-          $.each($('input[name="toppings"]:checked'), function() {
-            pizzaToppings.push($(this).val());
-          });
-
-          for (var i=0; i<pizzaToppings.length; i++) {
-            if (pizzaToppings[i] === "100") {
-              pizzaToppingsNames.push("Pepperoni")
-            }
-            else if (pizzaToppings[i] === "80") {
-              pizzaToppingsNames.push("Mushrooms")
-            }
-            else if (pizzaToppings[i] === "150") {
-              pizzaToppingsNames.push("Bacon")
-            }
-            else if (pizzaToppings[i] === "100") {
-              pizzaToppingsNames.push("Green Pepper")
-            }
-            else if (pizzaToppings[i] === "70") {
-              pizzaToppingsNames.push("Extra Cheese")
-            }
-            else if (pizzaToppings[i] === "90") {
-              pizzaToppingsNames.push("Pineapple")
-            }
-            else if (pizzaToppings[i] === "200") {
-              pizzaToppingsNames.push("Beef")
-            }
-            else if (pizzaToppings[i] === "210") {
-              pizzaToppingsNames.push("Ham")
-            }
-            else if (pizzaToppings[i] === "110") {
-              pizzaToppingsNames.push("Chicken")
-            }
-            toppingsCost += parseInt(pizzaToppings[i]);
-          }
-
-          var total = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(toppingsCost);
-          grandTotal += total;
-
-          let anotherPizza = new Pizza(pizzaSize, pizzaCrust, orderNo, total);
-
-          let anotherPizzaOrder = '<p>' + '<span id="orderNo">' + 'Order No: ' + anotherPizza.orderNo + ' | ' + '</span>' + '<span id="sizeOfPizza">' + 'Pizza Size: ' + $("#pizzaSize option:selected").text() + ' | ' + '</span>' + '<span id="crust">' + 'Pizza Crust: ' + $("#pizzaCrust option:selected").text() + ' | ' + '</span>' + '<span id="toppingsChosen">' + 'Toppings Chosen: ' + pizzaToppingsNames.join(", ") + ' | ' + '</span>' + '<span id="totalAmount">' + 'Total: ' + anotherPizza.total + '</span>' + '</p>'
-
-          $(".priceDisplay").append(anotherPizzaOrder);
-          $("#grandTotal").html('Grand total: ' + grandTotal);
-
-        });
       });
 
     });
